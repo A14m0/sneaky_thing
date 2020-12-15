681cd55c40f86a109ac9ae9d5a734ea1b1018e83
Probably Winston Churchill <nicolas.p.ammann@gmail.com>
2020-12-15 11:08:33 -0700

    Added some notes

--- smblib/src/lib.rs
+++ smblib/src/lib.rs
@@ -1,3 +1,9 @@
+
+// sources to look into structure
+// https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-smb2/5606ad47-5ee0-437a-817e-70c366052962
+
+
+
 #[cfg(test)]
 mod tests {
     #[test]
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,17 @@ use vm_detector::CheckVM;
 use std::{net::TcpStream, io::{Read,Write}};
 
 
+/// Eventually we wanna implement these things:
+/// 
+/// MS17-010 EternalBlue --> https://github.com/worawit/MS17-010/blob/master/eternalblue_exploit7.py
+/// CVE-2020-1350 WDNS Server RCE --> https://github.com/ZephrFish/CVE-2020-1350
+/// 
+/// Non-critical exploits we could use
+/// MS08-067 NETAPI --> https://github.com/fahrishb/Exploits/blob/master/MS08-067/ms08-067-poc.py
+/// 
+/// Additional features we should look into (totally not inspired by NotPetya...)
+/// Mimikatz password exposing --> https://github.com/gentilkiwi/mimikatz
+
 struct SneakiNet{
     id: u32, 
 }
