e112e8307aa8fc2b8c29aaffcea881dd1cb79803
Probably Winston Churchill <nicolas.p.ammann@gmail.com>
2020-12-17 15:03:44 -0700

    kind of fixed the ping thing

--- targeter/src/lib.rs
+++ targeter/src/lib.rs
@@ -9,15 +9,21 @@ pub mod targeter {
     use std::net::TcpStream;
     use std::str::FromStr;
 
+    use num::bigint::BigUint;
+    use num::One;
+    use num::ToPrimitive;
+    use core::ops::Add;
+
 
     #[derive(Debug)]
-    pub enum NetError {NoNet, NoValIP}
+    pub enum NetError {NoNet, NoValIP, IpDown}
 
     impl std::fmt::Display for NetError{
         fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
             match *self {
                 NetError::NoNet => write!(f, "No Network"),
                 NetError::NoValIP => write!(f, "No Valid IP Addresses Found"),
+                NetError::IpDown => write!(f, "Ip Is Down"),
             }
         }
     }
@@ -46,11 +52,14 @@ pub mod targeter {
             Ok(ip)
         }
 
-        fn check_target(&self, ip: &ipaddress::IPAddress) -> Result<u32, NetError> {
+        fn check_target(&self, ip: &ipaddress::IPAddress) -> 
+                                Result<ipaddress::IPAddress, NetError> {
+            let mut o = 0;
             if ip.is_network(){
                 println!("Network IP Address detected");
             } else {
-                println!("IP: {}", ip.to_s());
+                print!("IP: {}... \n", ip.to_s());
+                
                 let v4 = match Ipv4Addr::from_str(ip.to_s().as_str()){
                     Ok(i) => i,
                     Err(e) => panic!("Failed to convert address to IpvrAddr: {}", e)
@@ -59,29 +68,46 @@ pub mod targeter {
                 let t_ip = IpAddr::V4(v4);
                 
                 // determine if the target is up
-                let o = match ping::ping(t_ip, None, None, None, None, None) {
+                o = match ping::ping(t_ip, None, None, None, None, None) {
                     Ok(_) => {println!("Address is up!"); 0},
-                    Err(e) => {println!("Address is not up"); 1},
+                    Err(e) => {println!("\tdown ({})", e); 1},
                 };
             }
             
             
-            Ok(0)
+            Err(NetError::NoValIP)
         }
 
         // determine what IPs are valid on the subnet
         fn determine_ips(&mut self) -> Result<u32, NetError> {
             let locip = self.get_locip()?;
             let network = locip.network();
-            let mut loc_nets: Vec<String> = Vec::new();
             println!("Network: {}", network.to_s());
-            
 
-            network.each(|x| match self.check_target(x){
-                // need to find work around for non-mutable access to variables
-                Ok(_) => {loc_nets.append(&mut vec![x.to_s()]); ()},
-                Err(_) => ()
-            });
+            let x = &network.prefix;
+
+            let bc = match network.broadcast().host_address.to_u64(){
+                Some(x) => x,
+                None => 0,
+            };
+
+            let mut i = match network.host_address.to_u64(){
+                Some(x) => x,
+                None => 0,
+            };
+            
+            while i <= bc {
+                // do stuff
+                let re = &BigUint::from(i);
+
+                match self.check_target(&network.from(re, x)){
+                    // need to find work around for non-mutable access to variables
+                    Ok(x) => {self.valid_ips.push(x.to_s()); ()},
+                    Err(_) => ()
+                }
+
+                i += 1;
+            }
         
             Ok(0)
         }
